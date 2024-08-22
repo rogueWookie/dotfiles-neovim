e52605d44a5bc01c4fab880dd8dc106965109e8a
Shane Yost <syost@bluecanyontech.com>
2024-08-22 09:21:59 -0600

    added a todo in the markdown plugin config

--- lua/plugins/markmd-configs.lua
+++ lua/plugins/markmd-configs.lua
@@ -10,28 +10,31 @@ return {
 			})
 		end,
 	},
-    {
-        "MeanderingProgrammer/render-markdown.nvim",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-            local config = require("render-markdown")
-            config.setup({
-                link = {
-                    enabled = false,
-                }
-            })
-        end
-    },
-    {
-        "andrewferrier/wrapping.nvim",
-        config = function()
-            require("wrapping").setup({
-                notify_on_switch = true
-            })
-            vim.cmd("SoftWrapMode")
-        end
-    }
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			local config = require("render-markdown")
+			config.setup({
+				link = {
+					enabled = false,
+				},
+			})
+		end,
+	},
+    -- TODO     Supress wrapping mode from showing in stdout
+    -- TODO     Fix this undefined global issue, surpressing for now
+	{
+		"andrewferrier/wrapping.nvim",
+		config = function()
+			require("wrapping").setup({
+				notify_on_switch = true,
+			})
+			---@diagnostic disable-next-line: undefined-global
+			vim.cmd("SoftWrapMode")
+		end,
+	},
 }
