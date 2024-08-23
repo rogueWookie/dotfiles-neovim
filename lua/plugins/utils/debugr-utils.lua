78c89852171cbdb22a4778c6f7c2c961679e6510
Rogue Wookie <github@roguewookie.io>
2024-08-22 21:30:57 -0600

    adding whitespace cause Paul isnt here

--- lua/plugins/utils/debugr-utils.lua
+++ lua/plugins/utils/debugr-utils.lua
@@ -1,25 +1,31 @@
 -- NOTE     Overrides DAP UI's style symbols via creating a custom hl group
 --              - Overrides will dynamically adapt to changing colorschemes
+
 return {
 	setup = function()
 		local err_color = vim.api.nvim_get_hl_by_name("DapUIStop", true)
+
 		vim.api.nvim_set_hl(0, "DapBreakpoint", {
 			ctermbg = 0,
 			fg = string.format("#%06x", err_color.foreground),
 			bg = "none",
 		})
+
 		vim.fn.sign_define("DapBreakpoint", {
 			text = "",
 			texthl = "DapBreakpoint",
 			linehl = "",
 			numhl = "",
 		})
+
 		local exe_color = vim.api.nvim_get_hl_by_name("DapUIThread", true)
+
 		vim.api.nvim_set_hl(0, "DapStopped", {
 			ctermbg = 0,
 			fg = string.format("#%06x", exe_color.foreground),
 			bg = "none",
 		})
+
 		vim.fn.sign_define("DapStopped", {
 			text = "",
 			texthl = "DapStopped",
